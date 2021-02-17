283fc3dfaef3eeaf4022223b0bd44f50380023da
Enes C <mustafaenesceylan@hotmail.com>
2021-02-18 01:26:18 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Concrete.InMemory;
+using System;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        //SOLID
+        //Open Closed Principle
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(40,100)) 
+            {
+                Console.WriteLine(product.ProductName);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
