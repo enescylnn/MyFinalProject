37ec2264115df874a6e8f692f7b514a3023ec197
Enes C <mustafaenesceylan@hotmail.com>
2021-02-27 18:09:19 +0300

    My Final ProjectAdd project files.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,14 +11,31 @@ namespace ConsoleUI
         //Open Closed Principle
         static void Main(string[] args)
         {
+            //Data Transformation Object
+            ProductTest();
+            //IoC ile new'lemek düzelecek.
+            //CategoryTest();
+
+            Console.ReadLine();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine("Cateogry Name = " + category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(40,100)) 
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine("Product = " + product.ProductName + " --->"+ product.CategoryName);
             }
-            
-            Console.ReadLine();
         }
     }
 }
